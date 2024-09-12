068ebfe1b1535bb0323e12cb38ddead4434df83f
yjohnson67 <joh23004@byui.edu>
2024-09-11 22:35:06 -0500

    Explanation js

---
+++ explanation.js
@@ -0,0 +1,24 @@
+// Explanations
+var favColor = 'red';    
+//var is the variable which refers to something else
+//can not have spaces or dashes
+//value is assigned red but can be changed.
+//
+//text is called a string
+//
+var myArray = [];
+
+//Array is a list you can keep numbers, objects, text, variables, functions or whatever you want that is a list. 
+
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3; //If there are quotes it a text if not it is a number which can be manipulated with math.
+var hasGotFavColors = true; //boolean true of false or 0 or 1 or T or F (upper or lower case).  
+var richObject = {
+    firstName: 'Rich',
+    lastName: 'Armstrong',
+    favColors: ['blue', 'red', 'green'],
+
+    yearsAlive: 103,
+    isMale: true
+
+};
